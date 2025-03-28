b44557ef0e93dbbdd68e10fbc82fbf8cca07a45c
silvium04 <bundschuhsilva@gmail.com>
2025-03-28 22:45:38 +0100

    test stuff

--- backend/flashcardsonline/src/main/java/com/project/flashcardsonline/Controller/UsersController.java
+++ backend/flashcardsonline/src/main/java/com/project/flashcardsonline/Controller/UsersController.java
@@ -3,6 +3,7 @@ package com.project.flashcardsonline.Controller;
 import com.project.flashcardsonline.model.Users;
 import com.project.flashcardsonline.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,14 +16,14 @@ public class UsersController {
 	@Autowired
 	private UserService userService;
 
-	@PostMapping("/createUser")
+	@GetMapping("/createUser")
 	public Users createUser() {
 		Users user = new Users();
 		user.setUsername("test");
 		return userService.createUser(user);
 	}
 
-	@PostMapping("/getAllUsers")
+	@GetMapping("/getAllUsers")
 	public List<Users> getAllUsers() {
 		return userService.getAllUsers();
 	}
