95feee354b495ada44667a4f8bd258bd241bc6c6
oz-123 <oguzhancengiz1806@gmail.com>
2025-04-23 09:06:27 +0200

    kp

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import SideBySideDiv from './SidebySideDiv'; // Correct default import
 
@@ -11,18 +10,3 @@ const App = () => {
 };
 
 export default App;
-=======
-import logo from './logo.svg';
-import './App.css';
-import Flashcard from './copmonents/cards';
-
-function App() {
-  return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <Flashcard question="Was ist React?" answer="Eine JavaScript-Bibliothek für Benutzeroberflächen." />
-    </div>
-  );
-}
-
-export default App;
->>>>>>> dario/test
