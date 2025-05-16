96a185017b46dedbf258a28af5ec6241e08b26d5
Silva <bundschuhsilva@gmail.com>
2025-05-16 20:57:54 +0200

    idk2

---
+++ backend/flashcardsonline/src/test/java/com/project/flashcardsonline/Controller/LearningControllerTest.java
@@ -0,0 +1,33 @@
+package com.project.flashcardsonline.Controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class LearningControllerTest {
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@AfterEach
+	void tearDown() {
+	}
+
+	@Test
+	void getRandomOrderedDeck() {
+	}
+
+	@Test
+	void getBackwardsDeck() {
+	}
+
+	@Test
+	void getNormalDeck() {
+	}
+
+	@Test
+	void getLeitnerDeck() {
+	}
+}
